7c4cd502d74e2ddedf2fa8f15632da4caba83edb
AndreLuis7735 <andreluispaixaodeoliveira@gmail.com>
2026-03-22 21:05:31 -0300

    Add files via upload

---
+++ TodoParte/src/ClinicaMedica/Paciente.java
@@ -0,0 +1,48 @@
+package ClinicaMedica;
+
+public class Paciente {
+    private int id;
+    private String cpf, nome;
+
+    public Paciente() {
+    }
+
+    public Paciente(int id, String cpf, String nome) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
